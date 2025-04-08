1455960e4f16267c6199f8828d7b51fb8fc4b6b0
Stefan Kobel <stefankobel90@outlook.com>
2025-04-08 15:07:51 +0200

    Automatischer Commit von EZIO Gitwatch

--- capital_api.py
+++ capital_api.py
@@ -438,6 +438,7 @@ class CapitalComAPI:
         return 0.0
 
     def berechne_trade_groesse(self, epic: str, debug: bool = False, longterm: bool = False) -> float:
+        
         """
         Berechnet die empfohlene Tradegröße für ein EPIC anhand:
         - verfügbarem Kapital (in CHF)
